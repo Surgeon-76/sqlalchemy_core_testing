83356f9b6203fcfcb4c7d8d3b62b53d4afa80dd9
Surgeon76 <Surgeon76@yandex.ru>
2021-09-06 21:25:24 +0300

    1

--- my.py
+++ my.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+#from fastapi import FastAPI
+from sqlalchemy import *
 
-import sqlalchemy, databases
+import databases
 
 
+user_name_db = 'surglin'
+password_db = 'Nusha230399'
+db_name = 'alchemy_core_db'
+
+#database = databases.Database(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
+#metadata = sqlalchemy.MetaData()
+engine = create_engine(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
+engine.connect()
+print(engine)
